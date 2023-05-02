e904f1d87469441d164a884b7472518d020f00ee
KajalChauhan36 <131432927+KajalChauhan36@users.noreply.github.com>
2023-05-02 13:01:54 +0530

    Add project files.

---
+++ Git_DemoWebApplication1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Git_DemoWebApplication1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
